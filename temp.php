c119a9040eb5a09392945a4c4d280db8cff0c4e9
Tory Kulick <tory@kulick.io>
2018-11-18 17:43:13 -0500

    Started working on a PHP webhook listener

---
+++ temp.php
@@ -0,0 +1,25 @@
+<?php
+ if($json = json_decode(file_get_contents("php://input"), true)) {
+     print_r($json);
+     $data = $json;
+ } else {
+     print_r($_POST);
+     $data = $_POST;
+ }
+
+ echo "Saving data ...\n";
+ $url = "http://localhost:5984/incoming";
+
+ $meta = &#91;"received" => time(),
+     "status" => "new",
+     "agent" => $_SERVER['HTTP_USER_AGENT']];
+ $options = ["http" => [
+     "method" => "POST",
+     "header" => ["Content-Type: application/json"],
+     "content" => json_encode(["data" => $data, "meta" => $meta])]
+     ];
+
+ $context = stream_context_create($options);
+ $response = file_get_contents($url, false, $context);
+
+?>
