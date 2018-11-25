8895ad311e45b88d9b82a4cd290358ad87e390b6
Tory Kulick <30578137+tkulick@users.noreply.github.com>
2018-11-24 20:28:29 -0500

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
 temperature=`/home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 22 6`
-echo $temperature
 
-temp=`echo $temperature | sed -rn 's/Temp\=([0-9]+\.[0-9]+\s).*/\1/gp;'`
-echo $temp
+temp=`echo $temperature | sed -rn 's/Temp\=([0-9]+\.[0-9]).*/\1/gp;'`
 
 hum=`echo $temperature | sed -rn 's/.*Humidity\=([0-9]+\.[0-9]+).*/\1/gp;'`
-echo $hum
+
+request_body=$( cat << EOF
+{"temperature":"$temp","humidity":"$hum"}
+EOF
+)
 
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"temperature":"$temp","humidity":"$hum"}' \
+  --data $request_body \
   https://hooks.zapier.com/hooks/catch/2818261/ejif9i/
