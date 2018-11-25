0a57d1244e62c4ca53a24510081c32cab40626e0
Tory Kulick <30578137+tkulick@users.noreply.github.com>
2018-11-24 20:11:19 -0500

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+temperature=`/home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 22 6`
+echo $temperature
+
+temp=`echo $temperature | sed -rn 's/Temp\=([0-9]+\.[0-9]+\s).*/\1/gp;'`
+echo $temp
+
+hum=`echo $temperature | sed -rn 's/.*Humidity\=([0-9]+\.[0-9]+).*/\1/gp;'`
+echo $hum
+
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"temperature":"temp-here","humidity":"humidity-here"}' \
-  https://phoenix-omega.us/temperature/temp.php
+  --data '{"temperature":"$temp","humidity":"$hum"}' \
+  https://hooks.zapier.com/hooks/catch/2818261/ejif9i/
