bc72cc72a5098054015cac72c34dc45f24d57086
Tory Kulick <30578137+tkulick@users.noreply.github.com>
2019-01-27 19:00:24 -0500

    Added internal IP address lookup

--- temp.sh
+++ temp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-temperature=`/home/pi/temperature/dht-poll.py 22 6`
+temperature=`/home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 22 6`
 
 temp=`echo $temperature | sed -rn 's/Temp\=([0-9]+\.[0-9]).*/\1/gp;'`
 
@@ -7,8 +7,10 @@ hum=`echo $temperature | sed -rn 's/.*Humidity\=([0-9]+\.[0-9]+).*/\1/gp;'`
 
 ip=`curl https://ipinfo.io/ip`
 
+int=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+
 request_body=$( cat << EOF
-{"temperature":"$temp","humidity":"$hum","ip":"$ip"}
+{"temperature":"$temp","humidity":"$hum","ip":"$ip","internal":"$int"}
 EOF
 )
 
